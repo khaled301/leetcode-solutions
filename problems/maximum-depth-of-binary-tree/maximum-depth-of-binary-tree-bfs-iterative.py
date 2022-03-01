503708a2b77208d8f942595009727b002e9d0806
Khaled Hasan <khaledhasan301@gmail.com>
2022-03-01 12:15:31 +0600

    practised iterative BFS solution of maximum depth of the binary tree --- other

---
+++ problems/maximum-depth-of-binary-tree/maximum-depth-of-binary-tree-bfs-iterative.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        
+        level = 0
+        stack = deque([root])
+        
+        while stack:
+            for i in range(len(stack)):
+                node = stack.popleft()
+            
+                if node.left:
+                    stack.append(node.left)
+                if node.right:
+                    stack.append(node.right) 
+            level += 1
+            
+        return level
