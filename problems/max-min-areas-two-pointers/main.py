efe2069ee3741b14c842d30534957bde70326f54
Khaled Hasan <74279062+khaled-aiva@users.noreply.github.com>
2024-06-02 10:51:07 +0600

    added logic to find minimum area using the same two pointers approach

--- problems/max-min-areas-two-pointers/main.py
+++ problems/max-min-areas-two-pointers/main.py
@@ -28,10 +28,34 @@ class Solution:
                 right -= 1
 
         return max_area
+    
+    def minArea(self, height: list[int]) -> int:
+        min_area = float('inf')
+        left = 0
+        right = len(height) - 1
+
+        while left < right:
+            area = min(height[left], height[right]) * (right - left)
+            min_area = min(min_area, area)
+
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return int(min_area)
+
 
 new_sol = Solution()
-max_area = new_sol.maxArea([1,8,6,2,5,4,8,3,7])
-max_area2 = new_sol.maxArea2([1,1])
+max_area = new_sol.maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7])
+max_area2 = new_sol.maxArea2([1, 1])
+min_area = new_sol.minArea([1, 8, 6, 2, 5, 4, 8, 3, 7])
+min_area2 = new_sol.minArea([1, 1])
 
-print(max_area)
-print(max_area2)
+
+print(f"Max Area = {max_area}")
+print(f"Max Area 02 = {max_area2}")
+print(f"Min Area = {min_area}")
+print(f"Min Area 02 = {min_area2}")
+
+
