aa2dcbbca6cd2e386754a5df286b1c2f0d218401
Khaled Hasan <74279062+khaled-aiva@users.noreply.github.com>
2024-06-10 22:45:40 +0600

    Binary Search: updated the Optimal Solution for the binary search

---
+++ problems/binary-search/optimal-solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        if left == right and nums[left] == target:
+            return left
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return -1
