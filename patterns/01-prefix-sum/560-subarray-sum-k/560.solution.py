69602c3579e03ca0a36a5bbd36cd677f877fac17
Khaled Hasan <74279062+khaled-aiva@users.noreply.github.com>
2024-11-21 14:20:08 +0600

    leet 560: update the solution with example

--- patterns/01-prefix-sum/560-subarray-sum-k/560.solution.py
+++ patterns/01-prefix-sum/560-subarray-sum-k/560.solution.py
@@ -1,4 +1,5 @@
 
+from typing import List
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         totalSubArraysCount = 0
@@ -7,8 +8,17 @@ class Solution:
 
         for num in nums:
             currentSum += num
+            print(f"currentSum: {currentSum}")
             diffCurSumK = currentSum - k
+            print(f"diffCurSumK: {diffCurSumK}")
             totalSubArraysCount += prefixSumsDict.get(diffCurSumK, 0)
             prefixSumsDict[currentSum] = 1 + prefixSumsDict.get(currentSum, 0)
-        
+            print( prefixSumsDict )
+            print(f"totalSubArraysCount: {totalSubArraysCount}" )
+            print('---')        
         return totalSubArraysCount
+    
+sol = Solution()
+nums = [3, 5, 7, 2]
+k = 9
+print(sol.subarraySum(nums, k))
