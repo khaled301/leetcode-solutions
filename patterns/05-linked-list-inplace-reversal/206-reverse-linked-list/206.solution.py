5407d15f1546c7f0bd29c022de76fcd3576a8381
Khaled Hasan <74279062+khaled-aiva@users.noreply.github.com>
2024-11-22 13:01:23 +0600

    leet 206 - add recursive solution to the reverse linked list

--- patterns/05-linked-list-inplace-reversal/206-reverse-linked-list/206.solution.py
+++ patterns/05-linked-list-inplace-reversal/206-reverse-linked-list/206.solution.py
@@ -19,3 +19,26 @@ class Solution:
             curr = next 
             
         return prev
+    
+    # input 1-> 3 -> 13 -> None
+    def reverseListRecursive(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # Base case: if the list is empty or only one node remains
+        # Meet the condition at node 13 and returns 13->None as first reversedHead
+        if not head or not head.next:
+            return head 
+        
+        # Recursively reverse the rest of the list
+        reversedHead = self.reverseList(head.next)
+        
+        # Make the next node point to the current node
+        # after the first reversedHead "13->None" return the "head" will be in node 3->13 in "recursive" call
+        # and we need to move the node 3 to the position after the node 13,
+        # to look like 13 -> 3 ->... this
+        head.next.next = head 
+        
+        # Break the current node's next pointer to avoid cycles
+        # As the node 3 -> has a previous references of node 13, we can set the node 3 ->'s previous reference to None
+        head.next = None 
+        
+        # Return the reversedHead (1st: 13->None, 2nd: 13->3->None, 3rd: 13->3->1->None)
+        return reversedHead
