8e380828bec68f917b2daf16726a585dfd599fd0
Khaled Hasan <74279062+khaled-aiva@users.noreply.github.com>
2024-11-28 01:26:22 +0600

    leet 215: try quick select solution for kth largest element in a array and encounter time limit exceed issue

--- patterns/07-top-k-elements/215-kth-largest-element-in-array/215.solution.py
+++ patterns/07-top-k-elements/215-kth-largest-element-in-array/215.solution.py
@@ -62,8 +62,11 @@ class HeapQ:
 class Solution:
     # Input: nums = [3,2,1,5,6,4], k = 2
     # # Output: 5
+    def findKthLargestUsingSort(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        return nums[len(nums) - k]
 
-    def findKthLargest(self, nums: List[int], k: int) -> int:
+    def findKthLargestUsingMinHeap(self, nums: List[int], k: int) -> int:
         heap = nums[:k]
         heapQ = HeapQ(heap)
         heapQ.heapify()
@@ -75,8 +78,27 @@ class Solution:
         
         return heap[0]
 
+    # Will encounter Time Limit Exceeded for K >= 50000
+    def findKthLargestQuickSort(self, nums: List[int], k: int) -> int:
+        k = len(nums) - k 
+        
+        def quickSelect(l, r):
+            pivot, p = nums[r], l 
+            
+            for i in range(l, r):
+                if nums[i] <= pivot:
+                    nums[p], nums[i] = nums[i], nums[p]
+                    p += 1
+            nums[p], nums[r] = nums[r], nums[p]
+            
+            if p > k:   return quickSelect(l, p - 1)
+            elif p < k: return quickSelect(p + 1, r)
+            else:       return nums[p]
+
+        return quickSelect(0, len(nums) - 1)
+
 
 if __name__ == "__main__":
     sol = Solution()
-    print(sol.findKthLargest([3,2,1,5,6,4], 2))
-    print(sol.findKthLargest([3,2,3,1,2,4,5,5,6], 4))
+    print(sol.findKthLargestUsingMinHeap([3,2,1,5,6,4], 2))
+    print(sol.findKthLargestUsingMinHeap([3,2,3,1,2,4,5,5,6], 4))
