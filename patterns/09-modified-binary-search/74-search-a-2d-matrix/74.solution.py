7bfee064c3daa20833eba8c733f5bdff7cc1d352
Khaled Hasan <74279062+khaled-aiva@users.noreply.github.com>
2024-12-04 05:45:17 +0600

    leet 74: add optimal solution for search a 2d martrix for a target

--- patterns/09-modified-binary-search/74-search-a-2d-matrix/74.solution.py
+++ patterns/09-modified-binary-search/74-search-a-2d-matrix/74.solution.py
@@ -1,6 +1,26 @@
 from typing import List
 
 class Solution:
+    def searchMatrixOptimal(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix:
+            return False
+        
+        m, n = len(matrix), len(matrix[0])
+        l, r = 0, (m * n) - 1
+
+        while l <= r:
+            mid = l + (r - l) // 2
+            mid_row, mid_col = divmod(mid, n)
+            
+            if matrix[mid_row][mid_col] == target:
+                return True
+            elif matrix[mid_row][mid_col] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+                
+        return False
+    
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         if len(matrix) == 1:
             self.binarySearch(matrix[0], target)
